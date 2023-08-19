7da2671451aa61b7f9e181548c4ed66803d961d6
gopalbasak1 <gopalbasak1223@gmail.com>
2023-08-19 15:56:37 +0600

    add calculation entry

--- style/area-calculator.js
+++ style/area-calculator.js
@@ -12,12 +12,21 @@ function calculateTriangleButton(){
     const height = parseFloat(heightValueText);
     console.log(height);
 
+     //validation input : base || height
+     if(isNaN(base) || isNaN(height)){
+        alert('Please insert a number');
+        return
+    }
+
     const area = 0.5 * base * height;
     console.log(area)
 
     //show triangle area
     const areaSpan = document.getElementById('triangle-area');
     areaSpan.innerText = area;
+
+     //add to calculation entry
+     addToCalculationEntry('triangle', area)
 
 }
 
@@ -33,11 +42,21 @@ function calculateRectangleButton(){
     const length = parseFloat(lengthValueText);
     console.log(length);
 
+    //validation input : width || length
+    if(isNaN(width) || isNaN(length)){
+        alert('Please insert a number');
+        return
+    }
+
     const area = width * length;
    
 
     const areaSpan = document.getElementById('rectangle-area');
     areaSpan.innerText = area;
+
+    
+     //add to calculation entry
+     addToCalculationEntry('rectangle', area)
 }
 
 
@@ -52,14 +71,18 @@ function calculateRectangleButton(){
 //Parallelogram
 function calculateParallelogramButton(){
     const base = getInputValue('parallelogram-base');
-    console.log(base);
-
     const height = getInputValue('parallelogram-height');
-    console.log(height)
+    //validation input : base || height
+    if(isNaN(base) || isNaN(height)){
+        alert('Please insert a number');
+        return
+    }
 
     const area = base * height;
     setElementInnerText('parallelogram-area', area);
      
+    //add to calculation entry
+    addToCalculationEntry('parallelogram', area)
 }
 
 
@@ -67,8 +90,18 @@ function calculateParallelogramButton(){
 function calculateEllipseButton(){
     const majorRadius = getInputValue('ellipse-major-radius');
     const minorRadius = getInputValue('ellipse-minor-radius');
+     //validation input : base || height
+     if(isNaN(majorRadius) || isNaN(minorRadius)){
+        alert('Please insert a number');
+        return
+    }
+    
     const area = 3.14 * majorRadius * minorRadius;
-    setElementInnerText('ellipse-area', area)
+    const areaTwoDecimal = area.toFixed(2)
+    setElementInnerText('ellipse-area', areaTwoDecimal)
+
+     //add to calculation entry
+     addToCalculationEntry('ellipse', areaTwoDecimal)
     
 }
 
@@ -85,4 +118,41 @@ function getInputValue(fieldId){
 function setElementInnerText(elementId, area){
     const element = document.getElementById(elementId);
     element.innerText = area;
+}
+
+
+
+//Data validation
+/**
+ * 1. Set the proper type of the input field. (number,data,email)
+ * 2. check type using typeof
+ * 3. isNaN means: not a number. isNaN is checking wether the input is not a number or not.
+*/
+
+
+
+
+// Add to calculation entry 
+/**
+ * 1. get the element where you want to dynamic 
+ * 2. create an element you want to add
+ * 3. if needed add some class
+ * 4. set inner HTML. it could be dynamic Template string
+ * 5. append the created element as a child of the parent
+ */
+
+function addToCalculationEntry(areaType, area){
+    console.log(areaType+ ' ' +area)
+    const calculationEntry = document.getElementById('calculation-entry');
+
+    const count = calculationEntry.childElementCount;
+
+    const p = document.createElement('p');
+    p.classList.add('my-4') 
+    p.innerHTML =`${count + 1}. ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success">Convert</button>
+
+    `
+
+    calculationEntry.appendChild(p);
+
 }
