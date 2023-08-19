36316bbfcd05c9d61754cc7609966c944f199d62
gopalbasak1 <gopalbasak1223@gmail.com>
2023-08-19 10:11:58 +0600

    created html and javascript file and completed triangle and rectangle measurement completed

---
+++ style/area-calculator.js
@@ -0,0 +1,41 @@
+function calculateTriangleButton(){
+   
+    //get triangle base value
+    const baseField = document.getElementById('triangle-base');
+    const baseValueText = baseField.value;
+    const base = parseFloat(baseValueText);
+    console.log(base);
+
+    //get triangle height value
+    const heightField = document.getElementById('triangle-height');
+    const heightValueText = heightField.value;
+    const height = parseFloat(heightValueText);
+    console.log(height);
+
+    const area = 0.5 * base * height;
+    console.log(area)
+
+    //show triangle area
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
+
+}
+
+
+function calculateRectangleButton(){
+    const widthField = document.getElementById('rectangle-width');
+    const widthValueText = widthField.value;
+    const width = parseFloat(widthValueText);
+    console.log(width);
+
+    const lengthField = document.getElementById('rectangle-length');
+    const lengthValueText = lengthField.value;
+    const length = parseFloat(lengthValueText);
+    console.log(length);
+
+    const area = width * length;
+   
+
+    const areaSpan = document.getElementById('rectangle-area');
+    areaSpan.innerText = area;
+}
